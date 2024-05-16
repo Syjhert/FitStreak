b62ee3f2165d94a96b4b35b0a39fed58454959ed
Syjhert <robillos.jorashjonathan3@gmail.com>
2024-05-16 10:20:47 +0800

    template3

--- app/src/main/java/com/example/fitstreak/MainActivity.java
+++ app/src/main/java/com/example/fitstreak/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.fitstreak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout WaterLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WaterLayout = (LinearLayout) findViewById(R.id.WaterLayout);
+        WaterLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentToWaterActivity = new Intent(
+                        MainActivity.this, WaterActivity.class
+                );
+                startActivity(intentToWaterActivity);
+            }
+        });
     }
 }
